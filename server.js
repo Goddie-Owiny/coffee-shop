baea4fbd99d7f8f3f4c755f5b6dbbc3921051133
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2023-10-21 01:20:00 +0300

    Organized files for node

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const nodemailer = require("nodemailer")
 const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const path = require("path")
 const app = express()
 
 //middlewares
@@ -8,12 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render("index.ejs") 
 });
 
 
-
+ 
 const PORT = 1010;
 const port = process.env.port || 1010;
 
