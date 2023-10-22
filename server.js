4f7a8e523ee2aaa59198c67a6d836001643460ac
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2023-10-22 22:00:19 +0300

    Added the nodemailer

--- server.js
+++ server.js
@@ -21,10 +21,48 @@ app.get('/', (req, res) => {
     res.render("index.ejs") 
 });
 
+const contactAddress = 'goddiegodfreyz07@gmail.com';
 
+
+app.post('/contact', async (req, res, next) => {
+    
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "goddiegodfreyz07@gmail.com",
+            pass: "ptsayqzvfrwbwyoa",
+        },
+    });
+
+    const mailOption = {
+        //build email option
+        from: [req.body.email],
+        to: [contactAddress], 
+        subject: [req.body.content],
+        from: [req.body.name],
+        email: [req.body.email],
+        text: [req.body.content] 
+    } 
+
+    //send mail
+    transporter.sendMail(mailOption, (req, res, next, error, info) => {
+        if (error) {
+            console.log(error);
+            res.json({ msg: 'fail' });
+        } else {
+
+           console.log("Email sent to:" + contactAddress);
+            
+            // console.log(res);
+            // res.sendfile('/contact.ejs', { root: __dirname });
+        }
+    })
+    
+    res.redirect('/');
+});
  
 const PORT = 1010;
-const port = process.env.port || 1010;
+const port = process.env.port || 1010; 
 
  
 
